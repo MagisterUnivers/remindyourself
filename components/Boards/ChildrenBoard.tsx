e6f583e90d3d74b4fd9cf3feb2c038232be09b78
Andrii Shaposhnikov <droniiiandruxa@gmail.com>
2024-12-17 02:09:27 +0200

    ADD: ChildBoard markup

---
+++ components/Boards/ChildrenBoard.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { Eraser, Pencil } from "lucide-react"
+// import { useRouter } from "next/navigation"
+
+interface Props {
+  //
+}
+
+export const ChildrenBoard = ({ }: Props): React.ReactNode => {
+  // const router = useRouter()
+  return (
+    <li className="w-28 h-28 p-2 bg-gray-700 flex flex-col justify-between cursor-pointer text-white rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 ease-in-out">
+      <h2>Title child</h2>
+      <div className="flex gap-5 justify-center items-center">
+        <button
+          type="button"
+          title="Edit board"
+          className="cursor-pointer"
+        >
+          <Pencil />
+        </button>
+        <button
+          type="button"
+          title="Delete board"
+        >
+          <Eraser />
+        </button>
+      </div>
+    </li>
+  )
+}
--- components/Lists/ParentItemsHolder.tsx
+++ components/Lists/ParentItemsHolder.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-// import { useRouter } from "next/navigation"
 import { ParentBoard } from "../Boards/ParentBoard"
+// import { useRouter } from "next/navigation"
 
 interface Props {
   //
